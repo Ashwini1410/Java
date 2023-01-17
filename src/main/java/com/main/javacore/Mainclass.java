505689b83361c9cba997c83c7a045bc60bb416f9
Gashw <Gashw@LAPTOP-GQ4VDABC>
2023-01-17 14:15:46 +0530

    t

--- src/main/java/com/main/javacore/Mainclass.java
+++ src/main/java/com/main/javacore/Mainclass.java
@@ -11,6 +11,6 @@ package com.main.javacore;
  */
 public class Mainclass {
     public static void main(String[] args) {
-        System.out.println("hii");
+        System.out.println("99");
     }
 }
